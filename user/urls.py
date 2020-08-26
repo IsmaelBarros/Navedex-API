83f5dbaed0d1983d14663ff448f5008255040783
ismaelsb <ismasbarros2@gmail.com>
2020-08-25 23:26:46 -0300

    Added manage user endpoint

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,5 @@ app_name = 'user'
 urlpatterns = [
     path('signup', views.CreateUserView.as_view(), name='signup'),
     path('token', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
